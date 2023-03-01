89574ab176012eeac0378dc3c65c1b0e7fb4cc30
Robert Patterson <robert@robertgpatterson.com>
2023-03-01 09:35:56 -0600

    remove beta indicator from version string

--- src/luaosutils.hpp
+++ src/luaosutils.hpp
@@ -10,7 +10,7 @@
 #ifndef luaosutils_hpp
 #define luaosutils_hpp
 
-#define LUAOSUTILS_VERSION "Luaosutils 2.1.0.b3"
+#define LUAOSUTILS_VERSION "Luaosutils 2.1.0"
 
 #define MAC_OS       1         /* Macintosh operating system */
 #define WINDOWS      2         /* Microsoft Windows (MS-DOS) */
