f9c47cf121fd3456190ce074007863473bf56d28
Robert Patterson <robert@robertgpatterson.com>
2023-03-05 11:03:31 -0600

    add interest to process test

--- test/test-process-luaosutil.lua
+++ test/test-process-luaosutil.lua
@@ -33,9 +33,14 @@ if finenv.UI():IsOnWindows() then
     if true then
         -- local listing = process.execute('cmd /c chcp 65001 & /c REG QUERY \"HKLM\\Software\\Microsoft\" /reg:32')
         --local listing = process.execute('cmd /c chcp 65001 & REG QUERY \"HKCU\\SOFTWARE\\RGP\\Finale Plugin Prefs\" /reg:32')
-        --local listing = process.execute('cmd /c dir', finenv.RunningLuaFolderPath()) --"C:\\Program Files\\")
-        local listing = process.execute('cmd /c chcp & echo "∫üåé"')
+        --local listing, io_result = process_execute('dir "C:\\Program Files\\"')
+        local str1 = "ƒüåé"
+        local str1_850 = text.convert_encoding(str1, 65001, 850)
+        local str1_1252 = text.convert_encoding(str1, 65001, 1252)
+        print (str1, str1_850, str1_1252)
+        local listing, io_result = process_execute('echo "ƒüåé"')
         print(listing)
+        print(io_result)
         if listing then
             local fileout = io.open(finenv.RunningLuaFolderPath().."/".."listing.txt", "wb")
             fileout:write(listing)
