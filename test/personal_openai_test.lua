8aba24e774e7a5a837ea1395c77f548abe2220d1
Robert Patterson <robert@robertgpatterson.com>
2023-09-20 20:34:30 -0500

    test

--- test/personal_openai_test.lua
+++ test/personal_openai_test.lua
@@ -33,17 +33,17 @@ local openai = require("openai")
 
 openai.configure(openai_api_key)
 
-local model = "gpt-3.5-turbo"
+local model = "gpt-3.5-turbo" --"davinci"
 local prompt = "The quick brown fox jumps"
 local temperature = 1.5
 local max_tokens = 50
 
 function callback(download_successful, api_result)
+   local r = json.decode(api_result)
    if download_successful then
-       local r = json.decode(api_result)
        print(prompt..r.choices[1].text)
    else
-       print(api_result)
+       finenv.UI():AlertError(r.error.message, "OpenAI")
    end
    finenv.RetainLuaState = false
 end
